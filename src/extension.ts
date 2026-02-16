151a91ab610c38ceaaa3c9f64b1565e776f8d5c7
Roi Efrati <roie@edgify.ai>
2026-02-17 01:29:42 +0200

    basic link open

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,65 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
+function buildLink(
+  line: vscode.TextLine,
+  lineIndex: number,
+  packageName: string,
+) {
+  const startCharacter = line.text.indexOf(packageName);
+  const endCharacter = startCharacter + packageName.length;
+  const linkRange = new vscode.Range(
+    lineIndex,
+    startCharacter,
+    lineIndex,
+    endCharacter,
+  );
+  const registryUrl =
+    vscode.workspace.getConfiguration("npm-stuff").registryUrl;
+  const linkUri = vscode.Uri.parse(`${registryUrl}${packageName}`);
+  return new vscode.DocumentLink(linkRange, linkUri);
+}
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  const disposable = vscode.languages.registerDocumentLinkProvider(
+    { language: "json", pattern: "**/package.json" },
+    {
+      provideDocumentLinks(document) {
+        const links = [];
+        let lineIndex = 0;
+        let shouldCheckForDependency = false;
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "npm-stuff" is now active!');
+        while (lineIndex < document.lineCount) {
+          const line = document.lineAt(lineIndex);
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('npm-stuff.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from npm_stuff!');
-	});
+          if (shouldCheckForDependency) {
+            // no need to check for dependencies if block ended
+            if (line.text.includes("}")) {
+              shouldCheckForDependency = false;
+            } else {
+              // find dependency
+              const matches = line.text.match(/"(.*?)"/);
 
-	context.subscriptions.push(disposable);
+              if (matches) {
+                links.push(buildLink(line, lineIndex, matches[1]));
+              }
+            }
+          } else {
+            // check if we are in a dependencies block
+            shouldCheckForDependency = /"(.*?)dependencies"/i.test(line.text);
+          }
+
+          lineIndex += 1;
+        }
+
+        return links;
+      },
+    },
+  );
+
+  context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
